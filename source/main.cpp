0321cd3cd3639ef24875985331ff171b7eaab1a2
Wolfgange3311999 <matthew3311999@gmail.com>
2015-08-27 22:16:13 -0500

    Added example SDL main file

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,15 @@
 #include "graphics.h"
+#include <SDL.h>
 
 int main()
 {
-  displayMessage();
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
+
+	while (1)
+	{
+		SDL_RenderDrawPoint(renderer, 400, 300); //Renders on middle of screen.
+		SDL_RenderPresent(renderer);
+	}
 }
-
-
-/*SDL_Window *window;
-SDL_Renderer *renderer;
-SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
-
-//Probably on a loop
-  SDL_RenderDrawPoint(renderer, 400, 300); //Renders on middle of screen.
-  SDL_RenderPresent(renderer);*/
