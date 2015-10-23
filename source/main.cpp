0cd0499583fbbe8e72b3e9929e08900bff21975d
Wolfgange3311999 <matthew3311999@gmail.com>
2015-10-22 23:09:22 -0500

    Added dirty particle parallax demo

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,159 @@
-#include "graphics.h"
+#include <vector>
 #include <SDL.h>
+
+template<typename T> inline const T abs(T const & x)
+{
+	return ( x < 0) ? -x : x;
+}
+
+const int SIZE_X = 800, SIZE_Y = 600;
+const int MAX_PARTICLES = 200;
+
+class Particle
+{
+
+	enum Color
+	{
+		RED,
+		GREEN,
+		BLUE
+	};
+
+	enum Direction
+	{
+		LEFT,
+		RIGHT,
+		UP,
+		DOWN
+	};
+	int getRed(Color color)
+	{
+		switch(color)
+		{
+		case RED:
+			return 255;
+		case GREEN:
+		case BLUE:
+			return 0;
+		}
+	}
+	int getGreen(Color color)
+	{
+		switch(color)
+		{
+		case GREEN:
+			return 255;
+		case RED:
+		case BLUE:
+			return 0;
+		}
+	}
+	int getBlue(Color color)
+	{
+		switch(color)
+		{
+		case BLUE:
+			return 255;
+		case RED:
+		case GREEN:
+			return 0;
+		}
+	}
+
+	int x, y;
+	Direction dir;
+	Color color;
+	int delay;
+	Uint32 storedTime;
+
+	void changeColor()
+	{
+		switch (rand() % 3)
+		{
+		case 0:
+			color = RED;
+			break;
+		case 1:
+			color = GREEN;
+			break;
+		case 2:
+			color = BLUE;
+			break;
+		}
+	}
+
+	void changeDir()
+	{
+		switch (rand() % 4)
+		{
+		case 0:
+			dir = LEFT;
+			break;
+		case 1:
+			dir = RIGHT;
+			break;
+		case 2:
+			dir = UP;
+			break;
+		case 3:
+			dir = DOWN;
+			break;
+		}
+	}
+
+	bool inBounds()
+	{
+		return ((unsigned) x < SIZE_X)
+				&& ((unsigned) y < SIZE_Y);
+	}
+public:
+
+	bool update(Uint32 delta)
+	{
+		storedTime += delta;
+		if (storedTime > delay)
+		{
+			x += dir == LEFT ? -1 : (dir == RIGHT ? 1 : 0);
+			y += dir == UP ? -1 : (dir == DOWN ? 1 : 0);
+			storedTime -= delay;
+			return !inBounds();
+		}
+		return false;
+	}
+
+	void draw(SDL_Renderer *renderer)
+	{
+		SDL_SetRenderDrawColor(renderer, getRed(color), getGreen(color), getBlue(color), 255);
+		SDL_RenderDrawPoint(renderer, x, y);
+	}
+
+	Particle() : x(rand() % (SIZE_X / 4) + (SIZE_X * 3) / 8)
+	, y(rand() % (SIZE_Y / 4) + (SIZE_Y * 3) / 8), storedTime(0)
+	{
+		bool horizontal = abs(x - SIZE_X / 2) > abs(y - SIZE_Y / 2);
+		dir = horizontal ? (x < SIZE_X / 2 ? LEFT : RIGHT) : (y < SIZE_Y / 2 ? UP : DOWN);
+		delay = rand() % 60 + 10;
+		changeColor();
+	}
+};
 
 int main()
 {
 	SDL_Window *window;
 	SDL_Renderer *renderer;
-	//SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
-	window = SDL_CreateWindow("Prata", 10, 10, 800, 600, 0);
-
+	window = SDL_CreateWindow("Prata Demo", 10, 10, SIZE_X, SIZE_Y, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	std::vector<Particle> particles;
+	Sint32 ticksTillAdd = 1000;
+	Uint32 now, last = 0, delta = 0;
 
 	while (1)
 	{
+		now = SDL_GetTicks();
+		delta = now - last;
+		last = now;
+		ticksTillAdd -= delta;
 		SDL_Event e;
 		if (SDL_PollEvent(&e))
 		{
@@ -21,8 +163,25 @@ int main()
 			}
 		}
 
+		if (particles.size() < MAX_PARTICLES && ticksTillAdd < 100)
+		{
+			ticksTillAdd = 200;
+			particles.push_back(Particle());
+		}
+
 		SDL_RenderClear(renderer);
-		SDL_RenderDrawPoint(renderer, 400, 300); //Renders on middle of screen.
+		for (auto i = 0; i < particles.size(); ++i)
+		{
+			if (particles[i].update(delta))
+			{
+				particles.erase(particles.begin() + i);
+				printf("Deleted %d\nSize:%d\n", i, particles.size());
+				--i;
+				continue;
+			}
+			particles[i].draw(renderer);
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderPresent(renderer);
 	}
 
